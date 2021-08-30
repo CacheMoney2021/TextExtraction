bb89cbf60845d13e4e3ded250aa2a6ea53ac048f
aayesha1030 <62038400+aayesha1030@users.noreply.github.com>
2021-08-30 14:34:43 +1000

    Add files via upload

---
+++ attributeExtraction/AttributeExtraction.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 11:25:48 2021
+
+@author: aayes
+"""
+
+import pandas as pd
+import re
+Text_file = open('Part1.txt')
+Text = Text_file.read()
+
+#attribute lists to append
+referenceNo = []
+Location = []
+Provenance = []
+Height = []
+Diameter = []
+Plate = []
+Publications = []
+Description = []
+
+RecordList = Text.split('\n\n')
+RecordList.pop()
+#extracting reference number
+for record in RecordList:
+    refNo = record.split(sep=" ", maxsplit = 1)[0]
+    if refNo == '*':
+        refNo = record.split(sep=" ", maxsplit = 2)[1]
+        referenceNo.append('*' + refNo)
+    else:
+        referenceNo.append(refNo)    
+        
+#extractiing location:
+locRegex = r'Ht.|ht.|Ht|Diam. c.|Diam.|diam.|PLATE'
+startLine = '^\d+'
+
+for record in RecordList:
+      breakPoints = [' from ', ' Ht.', ' ht.', ' Ht', ' Diam. c.', ' Diam', ' diam', ' PLATE']
+      locationOnwards = record.split(sep=" ", maxsplit = 1)[1].strip()
+      if re.search(startLine, locationOnwards):
+          locationOnwards = locationOnwards.split(sep=" ", maxsplit = 1)[1].strip()
+      if any(breakPoint in record for breakPoint in breakPoints):
+            location = re.split(locRegex, locationOnwards)[0]
+            Location.append(location)
+      else:
+            location = locationOnwards.split(sep = '\n', maxsplit = 1)[0].strip()
+            Location.append(location)
+            
+for i in range(len(Location)):
+    if " from " in Location[i]:
+        Location[i] = Location[i].split(sep = " from ", maxsplit =1)[0]
+Location.pop()
+           
+# for record in RecordList:
+#     print(record)
+    
+# FirstLineList = []    
+# for record in RecordList:
+#     firstLine = record.split(sep = '\n', maxsplit = 1)[0].strip()
+#     refOnwards = firstLine.split(sep=" ", maxsplit = 1)
+#     FirstLineList.append(refOnwards)
+
+# A_list = []
+# FirstLineList.pop(-1)
+# for f_line in FirstLineList:
+#     A_list.append(f_line[1])
+
+# for F_line in A_list:
+#     if "from" in F_line:
+#         location = F_line.split(sep = " from", maxsplit = 1)[0].strip()
+#         Location.append(location)
+#     else:
+#         location = " "
+#         Location.append(location)
+        
+# for i in range(len(Location)):
+#     breakPoints = [' Ht.', ' ht.', ' Ht', ' Diam. c.', ' Diam', ' diam', ' PLATE']
+#     if Location[i] == " ":
+#         if any(breakPoint in RecordList[i] for breakPoint in breakPoints): 
+#             refOnward = RecordList[i].split(sep = " ", maxsplit = 1)[1].strip()
+#             #print(refOnward.split(sep =" ", maxsplit = 1)[0].strip())
+#             if refOnward.split(sep =" ", maxsplit = 1)[0].strip().isdigit(): #not working for record 100 but working for 101
+#                 refOnward = refOnward.split(sep = " ", maxsplit = 1)[1].strip()
+#             location = re.split(locRegex, refOnward)[0].strip()
+#             Location[i] = location
+#         else:
+#             refOnward = RecordList[i].split(sep = " ", maxsplit = 1)[1].strip()
+#             location = refOnward.split(sep = '\n', maxsplit = 1)[0].strip()
+#             Location[i] = location            
+          
+#print(FirstLineList)
+    #print(refOnwards)
+    # if "from" in refOnwards:
+    #     location = refOnwards.split(" from")[0].strip()
+    #     Location.append(location)
+    # elif any(breakPoint in record for breakPoint in breakPoints):
+    #     location = re.split(locRegex, record)[0].strip()
+    #     Location.append(location)
+    # else:
+    #     location = " "
+    #     Location.append(location)
+
+
+# #extracting Provenence
+proRegex = r'Ht.|ht.|Ht|Diam. c.|Diam.|diam.|PLATE'
+
+for record in RecordList:
+    breakPoints = [' Ht.', ' ht.', ' Ht', ' Diam. c.', ' Diam', ' diam', ' PLATE']
+    firstLine = record.split(sep = '\n', maxsplit = 1)[0]
+    if "from" in firstLine and any(breakPoint in record for breakPoint in breakPoints):
+        provenanceOnwards = record.split(sep="from", maxsplit = 1)[1].strip()
+        provenance = re.split(proRegex, provenanceOnwards)[0]
+        Provenance.append(provenance)
+    elif "from" in firstLine:
+        provenanceOnwards = firstLine.split(sep="from")[1].strip()
+        provenance = provenanceOnwards.split('\n', 1)[0]
+        Provenance.append(provenance)
+    else:
+        provenance = " "
+        Provenance.append(provenance)
+       
+       
+
+#extracting height
+for record in RecordList:
+    if "Ht." in record:
+        heightOnwards = record.split(sep="Ht.")[1]
+        HeightOnwardsStrip = heightOnwards.strip()
+        height = HeightOnwardsStrip.split(sep=" ", maxsplit = 1)[0]
+        Height.append(height)
+    elif "ht." in record:
+        heightOnwards = record.split(sep="ht.")[1]
+        HeightOnwardsStrip = heightOnwards.strip()
+        height = HeightOnwardsStrip.split(sep=" ", maxsplit = 1)[0]
+        Height.append(height)
+    elif "Ht" in record:
+        heightOnwards = record.split(sep="Ht")[1]
+        HeightOnwardsStrip = heightOnwards.strip()
+        height = HeightOnwardsStrip.split(sep=" ", maxsplit = 1)[0]
+        Height.append(height)
+    else:
+        height = " "
+        Height.append(height)
+
+#extracting diameter
+for record in RecordList:
+    if "Diam. c. " in record:
+        diamOnwards = record.split(sep="Diam. c. ")[1]
+        diamOnwardsStrip = diamOnwards.strip()
+        diameter = diamOnwardsStrip.split(sep=" ", maxsplit = 1)[0]
+        Diameter.append(diameter)
+    elif "Diam." in record:
+        diamOnwards = record.split(sep="Diam.")[1]
+        diamOnwardsStrip = diamOnwards.strip()
+        diameter = diamOnwardsStrip.split(sep=" ", maxsplit = 1)[0]
+        Diameter.append(diameter)
+    elif "diam." in record:
+        diamOnwards = record.split(sep="diam.")[1]
+        diamOnwardsStrip = diamOnwards.strip()
+        diameter = diamOnwardsStrip.split(sep=" ", maxsplit = 1)[0]
+        Diameter.append(diameter)
+    else:
+        diameter = " "
+        Diameter.append(diameter)
+
+#extracting plate
+for record in RecordList:
+    if "PLATE" in record:
+        plateOnwards = record.split(sep="PLATE")[1].strip()
+        plate = plateOnwards.split('\n', 1)[0]
+        Plate.append(plate)
+    else:
+        plate = " "
+        Plate.append(plate)
+        
+#extracting publication
+for record in RecordList:
+    pubs = []
+    firstSplit = record.split(sep='\n', maxsplit = 1)[1]
+    allPubs = re.split("\. \n", firstSplit)[0]
+    if allPubs[0] != "(":   #Potentially change this to an indent check for future Parts
+        pubs = allPubs.split(";")
+        publications = [p.replace("\n", "") for p in pubs] #removing \n from publications
+        Publications.append(publications)
+        #print(publications)
+    
+    else:
+        publications = " "
+        Publications.append(publications)
+        
+#extracting Desciption
+for record in RecordList:
+    if "(a)" in record :
+        description = record.split(sep = "(a)", maxsplit = 1)[1]
+        Description.append("(a) " + description)
+    elif "[a)" in record:
+        description = record.split(sep = "[a)", maxsplit = 1)[1]
+        Description.append("[a) " + description)
+    else:
+        firstSplit = record.split(sep='\n', maxsplit = 1)[1]
+        description = re.split("\. \n", firstSplit)
+        #description = firstSplit.split(sep = '\. \n', maxsplit = 1)
+        #description = firstSplit.partition("\. \n")[1]
+        Description.append(description)
+        
+
+Location.append(" ")
+print(len(referenceNo))
+print(len(Location))
+print(len(Provenance))
+print(len(Height))
+print(len(Diameter))
+print(len(Plate))
+print(len(Publications))
+print(len(Description))
+
+        
+#Writing DataFrames to a csv file
+# df = pd.DataFrame({'ReferenceNo': referenceNo,'Location': Location, 'Provenance': Provenance, 'Height': Height, 'Diameter': Diameter, 'Plate': Plate})
+# df.to_csv('ExtractedAttributes.csv', index=False)
