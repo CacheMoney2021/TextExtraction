1d4ac78c3fa907a9b3886050c440e5190a95629f
aayesha1030 <62038400+aayesha1030@users.noreply.github.com>
2021-09-04 00:06:20 +1000

    Update TextSizeFilter.py

--- Extraction code/TextSizeFilter.py
+++ Extraction code/TextSizeFilter.py
@@ -7,50 +7,47 @@ text = ''
 fileName = "RVP.pdf"
 pdf = pdfplumber.open(fileName)
 totalpages = len(pdf.pages)
-f = open("letter.txt", "a")
-
+# f = open("letter.txt", "a")
 MAX_TEXT_SIZE = 9.01
 MAX_INDENT = 61
 
+f = open("FOOTERrem.txt", "a")
 
+previous_height = 0
+current_height = 0
+count = 0
 
-for i in range(55, 82):
-        PageObj = pdf.pages[i]
-
-        for j in range(0, len(PageObj.chars)):
+for i in range(95,96):
+    write = True
+    PageObj = pdf.pages[i]
+    for j in range(0, len(PageObj.chars)):
+        if write:
+            current_height = PageObj.chars[j].get("top")
             if PageObj.chars[j].get("size") < MAX_TEXT_SIZE:
-                    Text = PageObj.chars[j].get("text")
-                    count = 0
+                Text = PageObj.chars[j].get("text")
+                size = str(PageObj.chars[j].get("size"))
+               # f2.writelines(Text + " " + size + '\n') 
+                if current_height != previous_height:
+                    #Text = PageObj.chars[j].get("text")
+                    Text = '\n'+Text
+                    #f.writelines(Text)
+                    previous_height = current_height
                     if PageObj.chars[j].get("x0") < MAX_INDENT:
                         text += '\n\n' + Text
-                        f.writelines('\n' + '\n' + Text)
+                        if 5.000 < PageObj.chars[j].get("size") < 6.000:
+                            write = False
+                            #print(PageObj.chars[j].get("size") , " " , PageObj.chars[j].get("text"))
+                        if write:
+                            f.writelines('\n'+Text)
+                            text = ""
                     else:
-                        text += Text + '\n'
-                        f.writelines(Text)
+                        if write:
+                            f.writelines(Text)
+                        #text += Text + '\n'
                         
-f.close()       
-
-newF = open('paragraphRemoved.txt', 'a')
-f = open("letter.txt")
-F = f.read()
-lines = F.split('\n')
-
-while("" in lines):
-    lines.remove("")
-    
-for i in range(0,len(lines)):
-    if len(lines[i])==1:
-        lines[i] = lines[i] + lines[i+1]
-        lines[i+1] = lines[i+1].replace(lines[i+1], "")
-
-while("" in lines):
-    lines.remove("")
-
-
-for line in lines:
-    newF.writelines(line+'\n\n')
-    
-newF.close()
+                else:
+                    if write:
+                        f.writelines(Text)
+        else:
+            break
 f.close()
-
-
