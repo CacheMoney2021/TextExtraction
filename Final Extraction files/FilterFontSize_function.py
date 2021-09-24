844fedeafebdbcaac39dfe4c1736c6420eeeb5e1
MoulinRogue <69712735+MoulinRogue@users.noreply.github.com>
2021-09-24 21:05:33 +1000

    Update FilterFontSize_function.py

--- Final Extraction files/FilterFontSize_function.py
+++ Final Extraction files/FilterFontSize_function.py
@@ -11,32 +11,42 @@ import pdfplumber
 fileName = "RVP.pdf"
 pdf = pdfplumber.open(fileName)
 totalpages = len(pdf.pages)
-MAX_TEXT_SIZE = 9.01 #For filtering out paragraph text
-MAX_INDENT = 61 #For checking new entries
+
+#For filtering out paragraph text
+MAX_TEXT_SIZE = 9.01 
+#For checking new entries
+MAX_INDENT = 61 
 
 def FilterFontSize(startPage, endPage, outputFile): 
     f = open(outputFile, "a")
     for i in range(startPage,endPage): #88-293 part 2
-        previous_height = 0 #to check for new lines
+        #to check for new lines
+        previous_height = 0 
         write = True
         PageObj = pdf.pages[i]
-        for j in range(0, len(PageObj.chars)): #while there are still characters to be read
+         
+        #continues while there are still characters to be read
+        for j in range(0, len(PageObj.chars)):
             if write:
                 current_height = PageObj.chars[j].get("top")
                 
-                if PageObj.chars[j].get("size") < MAX_TEXT_SIZE: #check if entry based on size
+                #check if entry based on size
+                if PageObj.chars[j].get("size") < MAX_TEXT_SIZE: 
                     Text = PageObj.chars[j].get("text")
                     
-                    if current_height > previous_height: #check if entry continues onto new line
+                    #check if entry continues onto new line
+                    if current_height > previous_height: 
                         Text = '\n' + Text
                         previous_height = current_height
                         
-                        if 5.000 < PageObj.chars[j].get("size") < 6.00: #check if footnote.
-                            write = False #do not write footnotes to output file
+                        #check if footnote.
+                        if 5.000 < PageObj.chars[j].get("size") < 6.00: 
+                            write = False 
                         
                         if write:
-                            if PageObj.chars[j].get("x0") < MAX_INDENT: #Check if beginning of new entry
-                                f.writelines('\n' + Text) #Add \n to mark new entry
+                            #Check if beginning of new entry
+                            if PageObj.chars[j].get("x0") < MAX_INDENT: 
+                                f.writelines('\n' + Text)
                             else:
                                 f.writelines(Text)
                     else:
