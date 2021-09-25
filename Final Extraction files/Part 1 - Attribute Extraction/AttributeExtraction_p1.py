c82c98ed1412a1186c8d52491acdcb8670b86267
MoulinRogue <69712735+MoulinRogue@users.noreply.github.com>
2021-09-25 14:04:33 +1000

    Code comments added
    Added up to Publication

--- Final Extraction files/Part 1 - Attribute Extraction/AttributeExtraction_p1.py
+++ Final Extraction files/Part 1 - Attribute Extraction/AttributeExtraction_p1.py
@@ -7,13 +7,12 @@ Created on Wed Sep  1 17:23:43 2021
 import re
 import pandas as pd
 
-
+#Creating list of Shapes to check against
 shape_file = open("VaseShapesReference.txt")
 shape = shape_file.read()
 ShapeList = shape.split('\n')
 for i in range(len(ShapeList)):
     ShapeList[i] = ShapeList[i].strip()
-    #print(shape)
 ShapeList.pop()
 
 regExp = '^(?!\d+|\*|\•).*'
@@ -21,19 +20,20 @@ f = open("Part1.txt")
 f_read = f.read()
 RecordList = f_read.split('\n\n')
 
-    
+#Labelling Shape in RecordList    
 for i in range(len(RecordList)):
     if re.search(regExp, RecordList[i]):
         if RecordList[i].upper().strip() in ShapeList:
             RecordList[i] = "SHAPE: " + RecordList[i]
-        #print(i)
+
         else:
             RecordList[i] = ""
 
 
 while("" in RecordList):
     RecordList.remove("")
- 
+
+#Attribute extraction    
 regFrom = r' from [A-Z]'
 for i in range(len(RecordList)):
     breakPoints = [' Ht.', ' ht.', ' Ht', ' Diam. c.', ' Diam', ' diam', ' PLATE', '(a)', '[a)', 'SHAPE: ']
@@ -102,30 +102,38 @@ for record in RecordList:
       #pastLocation code needs to be added
       breakPoints = [' from ', ' Ht.', ' ht.', ' Ht', ' Diam. c.', ' Diam', ' diam', ' PLATE']
       locationOnwards = record.split(sep=" ", maxsplit = 1)[1].strip()
-      if re.search(startLine, locationOnwards):
+        
+      #split on any Unicode decimal digit (Reference number)
+      if re.search(startLine, locationOnwards): 
           locationOnwards = locationOnwards.split(sep=" ", maxsplit = 1)[1].strip()
+      
+    #Break when any later attribute starts
       if any(breakPoint in record for breakPoint in breakPoints):
             location = re.split(locRegex, locationOnwards)[0]
             Location.append(location)
       else:
             location = locationOnwards.split(sep = '\n', maxsplit = 1)[0].strip()
             Location.append(location)
-            
+ 
+#Is this a double up?
 for i in range(len(Location)):
     if " from " in Location[i]:
         Location[i] = Location[i].split(sep = " from ", maxsplit =1)[0]
 
 
-#extracting Provenence
+#extracting Provenance
 proRegex = r'Ht.|ht.|Ht|Diam. c.|Diam.|diam.|PLATE'
 
 for record in RecordList:
     breakPoints = [' Ht.', ' ht.', ' Ht', ' Diam. c.', ' Diam', ' diam', ' PLATE']
     firstLine = record.split(sep = '\n', maxsplit = 1)[0]
+    
+    #Check if Provenance (preceeded by 'from') followed by other attributes
     if "from" in firstLine and any(breakPoint in record for breakPoint in breakPoints):
         provenanceOnwards = record.split(sep="from", maxsplit = 1)[1].strip()
         provenance = re.split(proRegex, provenanceOnwards)[0]
         Provenance.append(provenance)
+    #Check if Provenance
     elif "from" in firstLine:
         provenanceOnwards = firstLine.split(sep="from")[1].strip()
         provenance = provenanceOnwards.split('\n', 1)[0]
@@ -139,6 +147,8 @@ for record in RecordList:
 heights = r'Ht.|ht.|Ht'
 for record in RecordList:
     breakPoints = [' Ht.', ' ht.', ' Ht']
+    
+    #Check if Height attribute
     if any(breakPoint in record for breakPoint in breakPoints):
         HeightOnwards = re.split(heights, record)[1].strip()
         if HeightOnwards.startswith("("):
@@ -156,6 +166,8 @@ for record in RecordList:
 diams = r'diam. of base|Diam. c.|diam. c.|Diam.|diam.'
 for record in RecordList:
     breakPoints = [' diam. of base', ' Diam. c.', ' Diam.', ' diam.']
+    
+    #Check if Diameter attribute
     if any(breakPoint in record for breakPoint in breakPoints):
         DiamOnwards = re.split(diams, record)[1].strip()
         diameter = DiamOnwards.split(sep=" ", maxsplit = 1)[0]
@@ -169,6 +181,8 @@ for record in RecordList:
 plates = r'PLATES|PLATE'
 for record in RecordList:
     breakPoints = ['PLATES', 'PLATE']
+    
+    #Check if Plate
     if any(breakPoint in record for breakPoint in breakPoints):
         plateOnwards = re.split(plates, record)[1].strip()
         plate = plateOnwards.split('\n', 1)[0]
@@ -184,14 +198,17 @@ for record in RecordList:
     pubs = []
     checkPub = ' [0-9]+'
     removeList = ['above', '\\\\', ' \l.',' 1.', ' r.', '(a)', '[a)']
+    
+    #Publication never starts on first line. If no second line, no publications.
     if '\n' in record:
         firstSplit = record.split(sep='\n', maxsplit = 1)[1]
-        #if not firstSplit =="":
         allPubs = re.split("\. \n", firstSplit)[0]
+        #If Plate info is on second line, remove Plate. 
         if 'PLATE' in allPubs:
             allPubs = allPubs.split(sep='PLATE')[1]
             allPubs = (allPubs.split('\n')[1])
-    
+            
+        #If there are no numbers, there will be no publication.
         if re.search(checkPub, allPubs):
             if allPubs[0] != "(":   #Potentially change this to an indent check for future Parts
                 pubs = allPubs.split(";")
@@ -202,6 +219,8 @@ for record in RecordList:
     
                 for p in pubs:
                     removeIndex = []
+                    
+                    #removeList is words found ONLY in description
                     for r in removeList:
                         if r in p:
                             removeIndex.append(pubs.index(p)) #record 1/99 has an r. and isn't being removed.
