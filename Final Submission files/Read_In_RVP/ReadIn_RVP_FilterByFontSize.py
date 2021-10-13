b28966620c1d7515734d2311d2983f46fd42c7ef
aayesha1030 <62038400+aayesha1030@users.noreply.github.com>
2021-10-13 14:42:38 +1100

    Added comments

--- Final Submission files/Read_In_RVP/ReadIn_RVP_FilterByFontSize.py
+++ Final Submission files/Read_In_RVP/ReadIn_RVP_FilterByFontSize.py
@@ -9,7 +9,7 @@ fileName = "RVP.pdf"
 pdf = pdfplumber.open(fileName)
 totalpages = len(pdf.pages)
 MAX_TEXT_SIZE = 9.01 #max font size of all records 
-MAX_INDENT = 61 #All records are indented
+MAX_INDENT = 61 #Starting of all records are indented
 
 def FilterFontSize(startPage, endPage, outputFile):
     f = open(outputFile, "a")
@@ -40,6 +40,7 @@ def FilterFontSize(startPage, endPage, outputFile):
     f.close()  
     
 
+#Calling FilterFontSize function 3 times, for the 3 parts in the RVP with the specified startPage and endPage for each part 
 FilterFontSize(55, 82, "Part1.txt")
 FilterFontSize(88, 293, "Part2.txt")
 FilterFontSize(301, 394, "Part3.txt") 
